dd3541b89ad9512f7a99770bfbfa6b7a020a185d
giovanny-garcia <giovanny.garcia2@g.austincc.edu>
2024-10-06 16:01:35 -0600

    added bag.add and bag.remove

--- usingArrayBag.cpp
+++ usingArrayBag.cpp
@@ -5,16 +5,13 @@
 using namespace std;
 
 void sampleProgramFunction(ArrayBag<int> &bag);
-void menu();
+void menu(ArrayBag<int> &bag);
 void displayMenu();
 
 int main()
 {
 	// Array bag to hold integers
 	ArrayBag<int> bag;
-
-	//Integer to store selection
-	int selection;
 
 	// Array to hold initial values to put into the bag
 	int initialValues[] = {1, 33, 6, 9, 2, 65, 4, 29, 5, 8, 39, 88, 7, 25, 51, 3, 99, 14, 11, 10};
@@ -30,30 +27,8 @@ int main()
 	// some of the bag methods
 	sampleProgramFunction(bag);
 
-	
-	displayMenu();
-	do
-	{
-		switch (selection)
-		{
-		case 1:
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		case 4:
-			break;
-		case 5:
-			break;
-		case 6:
-			break;
-		default:
-			break;
-		}
-	}while(selection != 6);
 
-	menu();
+	menu(bag);
 	// Invoking the new method
 	// added as an example
 
@@ -61,12 +36,12 @@ int main()
 
 } // end main
 
-/****************************************************************/
-/* Function:   sampleProgramFunction
-/* Inputs:     the array bag
-/* Outputs:    none
-/* Purpose:    This function exercises some of the bag methods
-/****************************************************************/
+/* 
+Function:   sampleProgramFunction
+Inputs:     the array bag
+Outputs:    none
+Purpose:    This function exercises some of the bag methods
+*/
 
 void sampleProgramFunction(ArrayBag<int> &bag)
 {
@@ -78,9 +53,50 @@ void sampleProgramFunction(ArrayBag<int> &bag)
 	cout << "isEmpty: returns " << bag.isEmpty() << endl;
 } // end bagTester
 
-void menu()
+void menu(ArrayBag<int> &bag)
 {
+	int selection;	//Integer to store selection
 
+	do				//Loops through 
+	{
+		displayMenu();
+		std::cin >> selection;
+		switch (selection)
+		{
+		case 1:
+			int addValue;
+			cout << "Enter value: \n";
+			cin >> addValue;
+			bag.add(addValue);
+			cout << "Adding value " << addValue << endl;
+			break;
+			
+		case 2:
+			int removeValue;
+			cout << "Enter value to be removed: \n";
+			cin >> removeValue;
+			bag.remove(removeValue);
+			cout << "Removing value " << removeValue << endl;
+			break;
+		case 3:
+		{
+			std::vector<int> bagContents = bag.toVector();
+				for (std::size_t i = 0; i < bagContents.size(); i++) 
+				{
+        		std::cout << bagContents[i] << " ";
+				}
+		}
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		default:
+			break;
+		}
+	}while(selection != 6);
 }
 
 void displayMenu()
@@ -92,5 +108,5 @@ void displayMenu()
 	<< "4.) Find smallest value\n"
 	<< "5.) Find largest value\n"
 	<< "6.) Quit\n"
-	<< "Enter your choice: ";
+	<< "Enter your choice: \n";
 }
